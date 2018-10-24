9014c54de5f16d943d3ef19fc10ca2212dfedf4e
dispeter <leepeter718@gmail.com>
2018-10-24 13:45:09 +0800

    nestedloops

--- z- garbage/test 10.py
+++ z- garbage/test 10.py
@@ -1,29 +1,40 @@
 import pygame
-
-x = 0
-y = 0
-
-screen = pygame.display.set_mode((500, 500))
-
-running = True
+import random
+import time
+print("Pygame Color Changing Pyrimid")
+screen = pygame.display.set_mode((550,500))
+r=0
+g=0
+b=0
+color=[r,g,b]
+y=80
+running=True
+drawn=False
+screen.fill((255,255,255))
 while running:
-   for event in pygame.event.get():
-       if event.type == pygame.QUIT:
-           running = False
-
-   for x in range(0,600,100):
-       for y in range(0,600,100):
-           if (x==200 and x==y) or (x==400 and x==y) or (x==0 and x==y):
-               pygame.draw.rect(screen, (255, 255, 255), ((x,y), (100 , 100)))
-           elif (y==200 and y==400-x) or (y==400 and y==400-x) or (y==0 and y==400-x):
-               pygame.draw.rect(screen, (255, 255, 255), ((x, y), (100, 100)))
-           elif (x==300 and y%200==100) or (x==100 and y%200==100):
-               pygame.draw.rect(screen, (131, 0, 247), ((x, y), (100, 100)))
-           elif (x==200 and y%200==0):
-               pygame.draw.rect(screen, (255, 0, 0), ((x, y), (100, 100)))
-           elif (y==200 and x%200==0):
-               pygame.draw.rect(screen, (255, 0, 0), ((x, y), (100, 100)))
-
-
-   pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        #if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
+    if drawn==True:
+        y=80
+        for f in range(0,3):
+            n=random.randint(0,255)
+            color[f]=n
+        time.sleep(0.00000001)
+        drawn=False
+    while drawn==False:
+        for i in range(0,6):
+            x=0
+            for j in range(6-i):
+                x+=50
+            for j in range(1,i):
+                pygame.draw.rect(screen, color[0:], ((x, y), (50, 50)))
+                x+=50
+            for i in range(i, 0, -1):
+                pygame.draw.rect(screen, color[0:], ((x, y), (50, 50)))
+                x+=50
+            y+=50
+        drawn=True
+    pygame.display.update()
 pygame.quit()
